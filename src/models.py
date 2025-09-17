20ff8adf44e60a5b5994919c5921f8bcf37cfee0
Connor Wong <chocolatedounut@gmail.com>
2025-09-17 22:37:33 +0000

    took the models I used in my stawrwars blog REST API project and put it here

--- src/models.py
+++ src/models.py
@@ -1,19 +1,79 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+
+    favorites_list: Mapped[List["Favorites"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan")
+
+    # is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "favorites_list": [fav.serialize() for fav in self.favorites_list]
             # do not serialize the password, its a security breach
+        }
+
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    target_type: Mapped[str] = mapped_column(unique=False, nullable=False)
+    target_id: Mapped[int] = mapped_column(unique=False, nullable=False)
+    target_name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="favorites_list")
+
+    def get_target_favorite(self):
+        if self.target_type == "people":
+            return db.session.get(People, self.target_id)
+        elif self.target_type == "planets":
+            return db.session.get(Planets, self.target_id)
+        return None
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "target_type": self.target_type,
+            "target_id": self.target_id,
+            "target_name": self.target_name,
+            "user_id": self.user_id
+        }
+
+
+class People(db.Model):
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    hair_color: Mapped[str] = mapped_column(unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color
+        }
+
+
+class Planets(db.Model):
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    diameter: Mapped[str] = mapped_column(unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter
         }
